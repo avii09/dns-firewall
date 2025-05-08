8b27a4adeec59f4f72106aa8d48df427e69b352d
avii09 <avikesar2013@gmail.com>
2025-05-08 22:32:05 +0530

    feat: add main page with core functionality to initialize app

---
+++ simulator/main.py
@@ -0,0 +1,25 @@
+# main.py
+import os
+from query import launch_attack
+from drop_ip import drop_matched_ips
+from rate_limiter import rate_limit
+from filter import rules_match
+
+def main():
+    
+    query_result = launch_attack()
+
+    ratelim = rate_limit()
+
+    filterr = rules_match()
+
+    yara_matched = drop_matched_ips()
+    
+    
+    print(f"Query result saved to: logs/dns_query_log.csv {query_result}")
+    print(f"rate limit saved to: logs/rate_limiter_logs.csv {ratelim}")
+    print(f"Yara matched file saved to: logs/yara_matched.csv {filterr}")
+    print(f"legit ips saved to: logs/not_blocked.csv {yara_matched}")
+
+if __name__ == "__main__":
+    main()
